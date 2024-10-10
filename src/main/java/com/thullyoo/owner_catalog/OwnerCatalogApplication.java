1d3337446e90223cc6278cf0d5a6162c21cffb75
Thullyoo <thullyofilho@gmail.com>
2024-10-10 12:57:06 -0300

    Initial commit

---
+++ src/main/java/com/thullyoo/owner_catalog/OwnerCatalogApplication.java
@@ -0,0 +1,13 @@
+package com.thullyoo.owner_catalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnerCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwnerCatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thullyoo/owner_catalog/OwnerCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thullyoo.owner_catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwnerCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
